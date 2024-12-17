ba0202d791d7c6222d71659c0bf59843d0620b5f
Ryan James Gregoire <ryangregoire286@gmail.com>
2024-12-16 21:30:03 -0500

    Added Hex Color Code in RGBA Format

---
+++ RandomHexCodeGenerator/RandomHexCodeGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace App
+{
+    class App
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            
+            var rValue = r.Next(0, 255);
+            var bValue = r.Next(0, 255);
+            var cValue = r.Next(0, 255);
+            
+            Console.WriteLine(Color.FromArgb(rValue, bValue, cValue)); 
+            Console.ReadLine();
+
+        }
+    }
+}
+
